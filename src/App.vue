842d2dea09d1774c732816605a29c258e8495802
matmon12 <matveym151@gmail.com>
2024-05-15 13:49:22 +0300

    test

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
+
+<script setup></script>
+
+
+<style scoped lang="scss"></style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  Погода
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+</script>
+
+
+<style scoped lang="scss">
+</style>
