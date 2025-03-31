35b848806962174e2c626acdc0fd63e74e04c040
Matvey <matveym151@gmail.com>
2025-04-01 00:36:35 +0300

    fix request

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ export const CURRENT_URL = "https://api.weatherapi.com/v1/current.json";
 export const HISTORY_URL = "https://api.weatherapi.com/v1/history.json";
 export const ASTRO_URL = "https://api.weatherapi.com/v1/astronomy.json";
 export const FORECAST_URL = "https://api.weatherapi.com/v1//forecast.json";
-export const SEARCH_URL = "http://api.weatherapi.com/v1/search.json";
+export const SEARCH_URL = "https://api.weatherapi.com/v1/search.json";
 
 export const DEFAULT_LOCALE = "en";
 export const FALLBACK_LOCALE = "ru";
